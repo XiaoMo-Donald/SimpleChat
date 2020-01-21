7746bd41b44e058e407e03663f27a82e8b613f76
提拉米苏的呆猫 <49033970+Evan925@users.noreply.github.com>
2020-01-21 21:35:19 +0800

    Update helper.js

--- js/helper.js
+++ js/helper.js
@@ -39,13 +39,15 @@ const chatSetting = {
     },
     render: function() {
         var _options = this.get();
-        // console.log(_options)
-        if (_options.listAccordion) {
-            $("#UserFriendsList").attr('lay-accordion', '');
-        } else {
-            $("#UserFriendsList").removeAttr('lay-accordion');
+        console.log(_options);
+        if(_options!==null){ 
+            if (_options.listAccordion) {
+                $("#UserFriendsList").attr('lay-accordion', '');
+            } else {
+                $("#UserFriendsList").removeAttr('lay-accordion');
+            }
+            $("#ChatOpenPanelContent").css('background-image', `url(${_options.skin})`);
         }
-        $("#ChatOpenPanelContent").css('background-image', `url(${_options.skin})`);
     },
     init: function() {
         if (this.get() === null) {
